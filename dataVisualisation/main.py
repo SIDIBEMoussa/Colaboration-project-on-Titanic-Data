0c1904d513a1cfd7930f40b2d3d92997e55a1451
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-10-07 18:25:04 +0200

    small correction when the covid dataset is read on local machine

--- dataVisualisation/main.py
+++ dataVisualisation/main.py
@@ -106,7 +106,7 @@ def main():
         df = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
         df.to_csv("data/covidData.csv", sep=",", index=False)
     else:
-        df = pd.read_csv("covidData.csv")
+        df = pd.read_csv("data/covidData.csv")
 
 
     # 
